2a305f36f7e4c247b23159614af304a7fddf9491
--global <nvdai2401@gmail.com>
2020-08-31 17:00:05 +0700

    setup Image view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, withRouter } from 'react-router-dom';
+
+import { Image } from './views';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path='/' component={Image} />
+    </Switch>
   );
-}
+};
 
 export default App;
